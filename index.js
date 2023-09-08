8c4d668f7777b51f61a1f78f7ea405c4144523e4
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-09-07 18:56:48 -0600

    second function added

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-// First Class Function
+// First-class Function
 
 const uppercase = (string) => {
  return string.toUpperCase();
 }
 
 console.log(uppercase("Welcome"));
+
+
+//Higher-order Function
+
+const inputArray =  ['Hippopotamus', 'King Cobra', 'Giant Panda', 'Crocodile'];
+
+const newArray = inputArray.map(function(str) {
+return str.charAt(0).toLowerCase() + str.slice(1);
+});
+
+console.log(newArray);
+
+
+// const lowercase (func) => {
+//     return  (arr) => {
+
+//     }
+// }
+
+
+//Currying Function

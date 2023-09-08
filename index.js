3554170073d75676bdb53cb52568604504506729
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-09-07 19:15:37 -0600

    changes to second function

--- index.js
+++ index.js
@@ -9,20 +9,26 @@ console.log(uppercase("Welcome"));
 
 //Higher-order Function
 
-const inputArray =  ['Hippopotamus', 'King Cobra', 'Giant Panda', 'Crocodile'];
+const animals =  ['Hippopotamus', 'King Cobra', 'Giant Panda', 'Crocodile'];
 
-const newArray = inputArray.map(function(str) {
-return str.charAt(0).toLowerCase() + str.slice(1);
+// const newArray = inputArray.map(function(str) {
+// return str.charAt(0).toLowerCase() + str.slice(1);
+// });
+
+const newArray = (arr, func) => {
+    const array = [];
+    for(i = 0; i < arr.length; i++ ) {
+        array.push(func(arr[i]));
+    }
+        return array;
+}
+
+const modifiedArray = newArray(animals, function(a) {
+    return a.toLowerCase();
 });
 
-console.log(newArray);
-
-
-// const lowercase (func) => {
-//     return  (arr) => {
-
-//     }
-// }
+console.log(modifiedArray);
 
 
 //Currying Function
+

5468e22770611f785da89be802efb325e8177c14
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-09-10 12:35:46 -0600

    First class functions completed

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 // First-class Function
 
+//Problem 1:
 const uppercase = (string) => {
- return string.toUpperCase();
+    return string.toUpperCase();
 }
 
 console.log(uppercase("Welcome"));
 
+//Problem 2:
+const multiplier = (number1) => {
+    return function product(number2) {
+        return number1 * number2;
+    }
+}
+
+const multiply = multiplier(3);
+console.log(multiply(7));
+
+//Problem 3:
+
+const returnsAnObj = (num1, num2) => {
+    return {
+        1: num1,
+        2: num2
+    }
+
+}
+
+console.log(returnsAnObj(7, 9));
+
+
+
 
 //Higher-order Function
 
-const animals =  ['Hippopotamus', 'King Cobra', 'Giant Panda', 'Crocodile'];
+const animals = ['Hippopotamus', 'King Cobra', 'Giant Panda', 'Crocodile'];
 
 // const newArray = inputArray.map(function(str) {
 // return str.charAt(0).toLowerCase() + str.slice(1);
@@ -17,13 +42,13 @@ const animals =  ['Hippopotamus', 'King Cobra', 'Giant Panda', 'Crocodile'];
 
 const newArray = (arr, func) => {
     const array = [];
-    for(i = 0; i < arr.length; i++ ) {
+    for (i = 0; i < arr.length; i++) {
         array.push(func(arr[i]));
     }
-        return array;
+    return array;
 }
 
-const modifiedArray = newArray(animals, function(a) {
+const modifiedArray = newArray(animals, function (a) {
     return a.toLowerCase();
 });
 
@@ -31,4 +56,6 @@ console.log(modifiedArray);
 
 
 //Currying Function
+
+
 
